106b7f5e34eb367f48f1a394c3948b16aa891e59
tatojastine-n <jajanicoletato@gmail.com>
2025-08-07 12:34:57 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monthly_Sales_Tracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] months = { "January", "February", "March", "April", "May", "June",
+                           "July", "August", "September", "October", "November", "December" };
+
+            float[] sales = new float[12];
+            float totalSales = 0f;
+            int highestMonthIndex = 0;
+            int lowestMonthIndex = 0;
+
+            Console.WriteLine("Enter monthly sales for the year:");
+
+            for (int i = 0; i < sales.Length; i++)
+            {
+                Console.Write($"{months[i]}: ");
+                while (!float.TryParse(Console.ReadLine(), out sales[i]) || sales[i] < 0)
+                {
+                    Console.WriteLine("Invalid input. Please enter a positive number.");
+                    Console.Write($"{months[i]}: ");
+                }
+                totalSales += sales[i];
+            }
+            for (int i = 1; i < sales.Length; i++)
+            {
+                if (sales[i] > sales[highestMonthIndex])
+                    highestMonthIndex = i;
+
+                if (sales[i] < sales[lowestMonthIndex])
+                    lowestMonthIndex = i;
+            }
+            
+            float average = totalSales / sales.Length;
+
+            Console.WriteLine("\nSales Analysis Results:");
+            Console.WriteLine($"Highest Sales: {months[highestMonthIndex]} (${sales[highestMonthIndex]})");
+            Console.WriteLine($"Lowest Sales: {months[lowestMonthIndex]} (${sales[lowestMonthIndex]})");
+            Console.WriteLine($"Average Sales: ${average:F2}");
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Monthly Sales Tracker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Monthly Sales Tracker")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("88dacc60-a160-49a8-b569-ef0b595800fe")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
